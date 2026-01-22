adb4e0df5e28507548447a7af0fe2c3e9e89ea7b
cademtz <cadeemz@outlook.com>
2026-01-22 01:39:26 -0600

    Example of new and improved API

--- api.d.ts
+++ api.d.ts
@@ -1,17 +1,63 @@
 // This file provides documentation and auto-complete in IDEs like VSCode.
 // Editing this file has no effect.
 
-declare interface Game {
-    dialog: {
-        /** Slowly write text to screen */
-        write(text: string): void;
-        /** Writing sound */
-        sound(path: string): void;
-    },
-    /** Background image */
-    background(path: string): void;
-    /** Background music */
-    bgm(path: string): void;
+export type Color = string | number[]
+
+export interface Frame {
+    /** Seconds since game began */
+    time: number
+    /** Seconds since last frame */
+    deltaTime: number
 }
 
-export declare const game: Game;
+export interface Sprite {
+    x: number
+    y: number
+    color: Color
+}
+
+export type TextRender = (frame: Frame, letter: Sprite) => void
+
+export interface TextEffects {
+    speedMultiply?: number
+    color?: Color
+    render?: TextRender
+}
+
+export interface ChoiceMap {
+    [answer: string]: any
+}
+
+export interface Game {
+    dialog: {
+        /** Write new text and replace any old text */
+        say(text: string, options?: TextEffects): void
+        /** Write more text next to the old text */
+        more(text: string, options?: TextEffects): void
+        /** Add text. It is written immediately and silently. */
+        add(text: string, options?: TextEffects): void
+        /** Remove the last N characters */
+        removeLast(n: number): void
+        /** Erase all text */
+        clear(): void
+        /** Writing sound */
+        sound(path: string): void
+        /** Multiply the dialog speed */
+        speedMultiply(factor: number): void
+        /** Display choices on screen and wait for an answer */
+        choice(choices: ChoiceMap): any
+    },
+    /** Background image */
+    background(path: string): void
+    /** Background music */
+    bgm(path: string): void
+    /** Temporary hack to make colors, because arrays aren't supported yet :) */
+    rgba(r: number, g: number, b: number, a?: number): Color
+    /** Wait for a number of seconds. If no seconds are given then wait for any user input. */
+    wait(seconds?: number): void
+}
+
+export declare const game: Game
+export declare const say = game.dialog.say
+export declare const more = game.dialog.more
+export declare const add = game.dialog.add
--- main.js
+++ main.js
@@ -1,6 +1,73 @@
-import {game} from "./api.d.ts"
+import { game, say, more, add } from "./api.d.ts";
 
-game.bgm("sound/dos1.mp3")
+//game.bgm("sound/dos1.mp3")
+function wave(frame, letter) {
+    letter.color = game.rgba(
+        Math.sin(frame.time * 3 + letter.x/50 + letter.y) / 2 + 0.5,
+        Math.sin(frame.time * 3 + letter.x/50 + letter.y + Math.PI * 2/3) / 2 + 0.5,
+        Math.sin(frame.time * 3 + letter.x/50 + letter.y + Math.PI * 4/3) / 2 + 0.5,
+    )
+    letter.y += Math.cos(frame.time * 5 + letter.x/20 + letter.y) * 6
+}
+
+function shake(frame, letter) {
+    letter.x += Math.random() * 4 - 2
+    letter.y += Math.random() * 4 - 2
+}
+
+function bobLeft(frame, letter) {
+    letter.x -= Math.cos(frame.time * 1.5) * 16 / 2
+}
+
+function rest() {
+    add("  >", { render: bobLeft, color: "#0f08" })
+    game.wait()
+    game.dialog.removeLast(3)
+}
+
+// Start new dialog with *
+function undertale() {
+    game.dialog.clear()
+    add("* ")
+}
+
+const textEffects = {render: wave}
+
 game.background("img/bedroom.jpg")
-game.dialog.sound("sound/blip.wav")
-game.dialog.write("hallo! what's\ngoin\noooonnnnn???!!!")
+game.dialog.sound("sound/squeek.wav")
+
+let answer = null
+while (answer == null) {
+    say("minion or da bob fo today")
+    answer = game.dialog.choice({
+        "Minion plz": "minion!!!",
+        "Bob! bob!": "bob???",
+        neither: null,
+    })
+    if (answer == null) {
+        say("no!!!")
+        rest()
+    }
+}
+
+say(answer)
+rest()
+
+undertale()
+more("hallo!")
+rest()
+
+undertale()
+more("what's\ngoin\n", {speedMultiply: 0.5})
+game.wait(0.1)
+more("oooonnnnn???!!!??????????????????\n", textEffects)
+rest()
+
+more("... ", { speedMultiply: 0.2 })
+game.wait(0.5)
+more("Let's change the effects\n", {color: "#ff0"})
+game.wait(1)
+// Cool trick: We can edit textEffects later whenever we wish
+textEffects.color = "#0f0"
+textEffects.render = shake
+game.wait()
